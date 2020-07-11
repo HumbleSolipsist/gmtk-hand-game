09baff0fd401af9524c59d4ee425cc43e9900df5
Andrew Busto <andrewlbusto@gmail.com>
2020-07-10 23:12:31 -0700

    Add basic click and drag functionality to the hand.

--- LeftHand.gd
+++ LeftHand.gd
@@ -1,14 +1,22 @@
 extends Area2D
 
-export (PackedScene) var Lethal
+var mouse_hover = false
+var follow_mouse = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-#func get_input():
-#	if Input.is_action_just_pressed('ui_left_click'):
-#		$drag_hitbox.
+func _process(delta):
+	if follow_mouse:
+		self.position = get_viewport().get_mouse_position()
+	
+func _input(event):
+	if mouse_hover and event is InputEventMouseButton:
+		follow_mouse = not follow_mouse
+
+func _on_pointer_area_entered(area):
+	mouse_hover = true
+
+func _on_pointer_area_exited(area):
+	mouse_hover = false
