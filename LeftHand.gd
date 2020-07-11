df6779a7b5c2936aadda491520aa04574f138d94
Andrew Busto <andrewlbusto@gmail.com>
2020-07-10 23:43:10 -0700

    Limit hand speed.

--- LeftHand.gd
+++ LeftHand.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+export (float) var active_speed
+export (float) var passive_speed
+export (Vector2) var home
+
 var mouse_hover = false
 var follow_mouse = false
 
@@ -7,9 +11,24 @@ var follow_mouse = false
 func _ready():
 	pass # Replace with function body.
 
-func _process(delta):
+func travel_to(destination, speed):
+	if destination.distance_to(self.position) > speed:
+		var velocity = (destination - self.position).normalized() * speed
+		self.position = self.position + velocity
+	else:
+		self.position = destination
+
+func go_to_mouse():
+	self.travel_to(get_viewport().get_mouse_position(), active_speed)
+
+func go_home():
+	self.travel_to(self.home, passive_speed)
+
+func _physics_process(delta):
 	if follow_mouse:
-		self.position = get_viewport().get_mouse_position()
+		self.go_to_mouse()
+	else:
+		self.go_home()
 	
 func _input(event):
 	if mouse_hover and event is InputEventMouseButton:
