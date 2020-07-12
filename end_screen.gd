aaf0415cd2402a601013e34efdda0e27bbfad81e
KarlTheCool <k@rlphilli.ps>
2020-07-12 03:37:08 -0700

    added reset

--- end_screen.gd
+++ end_screen.gd
@@ -10,3 +10,8 @@ func _process(delta):
 		get_node("../bg_music").playing = false
 		get_node("./lose_sfx").playing = true
 		oneshot = true;
+
+func _input(ev):
+	if (ev is InputEventKey and ev.pressed) or (ev is InputEventMouseButton and ev.pressed):
+		if oneshot:
+			get_tree().reload_current_scene()
