2615fc63ee56090f7b3f1d4f1327c9d73e42c297
KarlTheCool <k@rlphilli.ps>
2020-07-12 00:30:18 -0700

    da shakes

--- Enemy.gd
+++ Enemy.gd
@@ -12,6 +12,8 @@ enum STATE {
 
 var state = STATE.idle
 
+var shakes = 0;
+
 func _ready():
 	pass # Replace with function body.
 
@@ -23,12 +25,19 @@ func _process(delta):
 		ouch_sprite.visible = false;
 		idle_sprite.visible = true;
 
+func _physics_process(delta):
+	$bug_ouch.offset = Vector2(rand_range(-shakes, shakes), rand_range(-shakes, shakes));
+	$bug_stand.offset = Vector2(rand_range(-shakes, shakes), rand_range(-shakes, shakes));
+	shakes = max(shakes * 0.75, 0);
+
 func hand_over(hand):
 	state = STATE.hurt
 	if (hand.item):
 		hand.item.hit(self)
+		shakes = shakes + 50;
 	else:
 		$smack_sound.play()
+		shakes = shakes + 25;
 		hand.close_hand()
 
 func hand_off(hand):
