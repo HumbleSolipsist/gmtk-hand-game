a2cfe3e9ee917643274838404b4f03bbeae62e6e
Andrew Busto <andrewlbusto@gmail.com>
2020-07-11 23:59:29 -0700

    Add item spawn with time.

--- World.gd
+++ World.gd
@@ -1,15 +1,32 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const staff_gap = 5.0
+const potion_gap = 3.0
+
+onready var time_to_staff = staff_gap
+onready var time_to_potion = potion_gap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$inventory.add($potion)
 	randomize()
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func make_new_item(path):
+	var new_item = load(path).instance()
+	self.add_child(new_item)
+	
+	var itemx = rand_range(0, 1024)
+	var itemy = rand_range(550, 550 - (sin(PI * itemx/1024) * 100))
+	new_item.position = Vector2(itemx, itemy)
+	
+	return new_item
+
+func _physics_process(delta):
+	time_to_potion -= delta
+	time_to_staff -= delta
+	if (time_to_potion <= 0):
+		time_to_potion += potion_gap
+		self.make_new_item("res://potion.tscn")
+	if (time_to_staff <= 0):
+		time_to_staff += potion_gap
+		var item = self.make_new_item("res://staff.tscn")
